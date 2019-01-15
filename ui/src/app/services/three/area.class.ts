f5a8b9c6ad054296b4a5c285d1129f172a5df971
Yannick Roffin <yroffin@gmail.com>
2019-01-15 23:55:43 +0100

    Update

--- ui/src/app/services/three/area.class.ts
+++ ui/src/app/services/three/area.class.ts
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { Point } from 'paper';
+import * as _ from 'lodash';
 
 export class AreaPoint {
     public origin: Point;
@@ -10,25 +11,25 @@ export class Area {
     public name: string;
     public meshes: THREE.Object3D[];
 
-    public _points: AreaPoint[];
+    private internalPoints: AreaPoint[];
 
     public isBound = false;
 
     public constructor(name: string) {
         this.name = name;
         this.meshes = [];
-        this._points = [];
+        this.internalPoints = [];
     }
 
     public add(px: number, py: number, nx: number, ny: number) {
-        this._points.push({
+        this.internalPoints.push({
             origin: new Point(px, py),
             normal: new Point(nx, ny)
         });
     }
 
     public points() {
-        return this._points;
+        return this.internalPoints;
     }
 
     public isOpen(): boolean {
@@ -37,7 +38,7 @@ export class Area {
 
     private area(): number {
         const shape: Array<THREE.Vector2> = [];
-        _.each(this._points, (point: AreaPoint) => {
+        _.each(this.internalPoints, (point: AreaPoint) => {
             shape.push(new THREE.Vector2(point.origin.x, point.origin.y));
         });
         return Math.abs(THREE.ShapeUtils.area(shape));
@@ -45,7 +46,7 @@ export class Area {
 
     private areaNormal(): number {
         const shape: Array<THREE.Vector2> = [];
-        _.each(this._points, (point: AreaPoint) => {
+        _.each(this.internalPoints, (point: AreaPoint) => {
             shape.push(new THREE.Vector2(point.origin.x + point.normal.x, point.origin.y + point.normal.y));
         });
         return Math.abs(THREE.ShapeUtils.area(shape));
